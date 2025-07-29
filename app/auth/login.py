96cbe116035756f151ffb1a1c435ae994aebee3d
snowleopard618 <snowleo1342@gmail.com>
2025-07-29 15:20:29 +0900

    Update login.py

--- app/auth/login.py
+++ app/auth/login.py
@@ -15,7 +15,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=15)):
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=45)):
     to_encode = data.copy()
     expire = datetime.utcnow() + expires_delta
     to_encode.update({"exp": expire})
